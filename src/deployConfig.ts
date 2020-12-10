402f6bf508826f6c4998c0a0a8d4f771b4fe2399
Krzysztof Warzecha <kw@gamerhash.com>
2020-12-10 18:34:45 +0100

    Add start time for locking contracts

--- src/deployConfig.ts
+++ src/deployConfig.ts
@@ -3,7 +3,9 @@ import {parseTokens} from './utils/parseTokens'
 import {currentTimeSeconds} from './utils/currentTimeSeconds'
 import {DAY, GREGORIAN_MONTH, GREGORIAN_YEAR, HOUR} from './utils/timeConstants'
 
-// TODO set desired start times
+// this is in unix time, unix time 1609369200 is Wed Dec 30 23:00:00 UTC 2020
+// confirm with `date --date=@1609369200`
+export const START_TIME = 1609369200;
 
 export const deployConfig: DeploymentParams = {
   totalTokenSupply: parseTokens(880_000_000),
@@ -25,7 +27,7 @@ export const deployConfig: DeploymentParams = {
       lockedAmount: parseTokens(132_000_000),
       beneficiaryAddress: '0xCfC99a10332D4992a9a96485a577F6c6dF77Ea98',
       releaseSchedule: {
-        startTime: currentTimeSeconds() + HOUR,
+        startTime: START_TIME,
         cliffDuration: GREGORIAN_YEAR,
         cliffAmount: parseTokens(13_200_000),
         numSteps: 18,
@@ -38,7 +40,7 @@ export const deployConfig: DeploymentParams = {
       lockedAmount: parseTokens(44_000_000),
       beneficiaryAddress: '0x96e5fc4c3B30164c9eAF5a6d8b176973995EC379',
       releaseSchedule: {
-        startTime: currentTimeSeconds() + HOUR,
+        startTime: START_TIME,
         cliffDuration: 30 * DAY,
         cliffAmount: parseTokens(1_760_000),
         numSteps: 32,
@@ -51,7 +53,7 @@ export const deployConfig: DeploymentParams = {
       lockedAmount: parseTokens(123_200_000),
       beneficiaryAddress: '0x72AE2C5A9985b5DAb9a5a6C134763Eb4247F3b6e',
       releaseSchedule: {
-        startTime: currentTimeSeconds() + HOUR,
+        startTime: START_TIME,
         cliffDuration: GREGORIAN_YEAR,
         cliffAmount: parseTokens(30_800_000),
         numSteps: 3,
@@ -64,7 +66,7 @@ export const deployConfig: DeploymentParams = {
       lockedAmount: parseTokens(35_200_000),
       beneficiaryAddress: '0xDF6B2dDEE4FBACbdb5045327176454963c7f0069',
       releaseSchedule: {
-        startTime: currentTimeSeconds() + HOUR,
+        startTime: START_TIME,
         cliffDuration: 90 * DAY,
         cliffAmount: parseTokens(8_800_000),
         numSteps: 3,
@@ -77,7 +79,7 @@ export const deployConfig: DeploymentParams = {
       lockedAmount: parseTokens(105_600_000),
       beneficiaryAddress: '0x7B4395Eec8Ac4196Fdd1F9C4d057Afc636deAA79',
       releaseSchedule: {
-        startTime: currentTimeSeconds() + HOUR,
+        startTime: START_TIME,
         cliffDuration: 30 * DAY,
         cliffAmount: parseTokens(10_560_000),
         numSteps: 5,
@@ -90,7 +92,7 @@ export const deployConfig: DeploymentParams = {
       lockedAmount: parseTokens(35_200_000),
       beneficiaryAddress: '0x577584EeBF32F0863202b5166c2Bb9738e1A3601',
       releaseSchedule: {
-        startTime: currentTimeSeconds() + HOUR,
+        startTime: START_TIME,
         cliffDuration: 90 * DAY,
         cliffAmount: parseTokens(8_800_000),
         numSteps: 3,
@@ -103,7 +105,7 @@ export const deployConfig: DeploymentParams = {
       lockedAmount: parseTokens(44_000_000),
       beneficiaryAddress: '0xb1a17849627480eb750CbF9A75798C0036308c9F',
       releaseSchedule: {
-        startTime: currentTimeSeconds() + HOUR,
+        startTime: START_TIME,
         cliffDuration: 2 * GREGORIAN_YEAR,
         cliffAmount: parseTokens(22_000_000),
         numSteps: 1,
