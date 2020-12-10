7be82dcf656fcdac37b042f903b4b45e2593380c
Krzysztof Warzecha <kw@gamerhash.com>
2020-12-10 19:44:42 +0100

    Change default wallet and network provider
    During test token deployment we've got few exceptions that were related to
    default FallbackProvider.
    
    https://docs.ethers.io/v5/api/providers/other/#FallbackProvider
    
    > When a request is made, the request is dispatched to multiple
    > backends, randomly chosen (higher priority backends are always selected
    > first) and the results from each are compared against the others. Only
    > once the quorum has been reached will that result be accepted and
    > returned to the caller.
    
    > By default the quorum requires 50% (rounded up) of the backends to
    > agree. The weight can be used to give a backend Provider more influence.
    
    They did not agree. We are using only infura.
    
    As for wallet provider, we are using the same wallet format as geth
    because we need this wallet for verification on etherscan.

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,27 @@
 import * as fs from 'fs'
-import {getDefaultProvider, Wallet} from 'ethers'
+import {providers, utils, Wallet} from 'ethers'
 import {deployGamerCoin} from './deployGamerCoin'
 import {deployConfig} from './deployConfig'
 import {consoleLogger} from './model/Logger'
 
 async function run(args: string[]) {
-  if (args.length !== 1) {
+  if (args.length !== 2) {
     throw new Error('Invalid number of arguments')
   }
   const secretsFile = fs.readFileSync(args[0], 'utf8')
   const secrets = JSON.parse(secretsFile)
 
-  const provider = getDefaultProvider()
-  const wallet = new Wallet(secrets.privateKey, provider)
+  const network = "homestead"
+  const provider = new providers.InfuraProvider(network, {
+    projectId: '',
+    projectSecret: '',
+  })
+  const wallet = Wallet.fromEncryptedJsonSync(secretsFile, args[1]).connect(provider)
+
+  consoleLogger.info(`Opened wallet ${wallet.address}, checking balance...`);
+  let balance = await wallet.getBalance();
+  consoleLogger.info(`network says we've got ${utils.formatEther(balance)} eth (${balance} wei)`);
+  consoleLogger.info(`current gas price: ${utils.formatUnits(await provider.getGasPrice(), 'gwei')} gwei - compare this with https://ethgasstation.info/`);
   await deployGamerCoin(wallet, deployConfig, consoleLogger)
 }
 
